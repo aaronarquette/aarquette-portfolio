e8220ddb0a1339d5f64232725263bdcf135ebfa1
aaronarquette <aaronarquette538@outlook.com>
2026-06-10 00:17:25 +0700

    feat: add about me section and set height to screen height

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
+// import Image from "next/image";
+
 export default function Home() {
   return (
     <>
-      {/* //TODO: Make the total height of navbar and hero component be the height of the screen */}
       <section className="hero bg-base-200 min-h-screen">
         <div className="hero-content text-center">
           <div className="max-w-md">
             <h1 className="text-5xl font-bold">Aaron Arquette</h1>
             <p className="py-6">Full-Stack Developer</p>
             <button className="btn btn-primary">Start</button>
+          </div>
+        </div>
+      </section>
+      <section className="hero bg-base-100 h-screen">
+        <div className="hero-content flex-col">
+          {/* <Image></Image> */}
+          <div>
+            <h1 className="text-5xl font-bold">About Me</h1>
+            <p className="py-6">An inspiring full-stack developer</p>
           </div>
         </div>
       </section>
