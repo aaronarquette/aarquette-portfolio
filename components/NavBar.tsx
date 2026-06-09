f918a1b9f2f893f1e9e1431f051311e91d1d4dcd
aaronarquette <aaronarquette538@outlook.com>
2026-06-10 00:15:40 +0700

    feat: add frosted glass design and centered navigation buttons

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,20 +4,17 @@ export default function NavBar() {
   // About, Projects, Tech Stacks
   return (
     <>
-      <nav className="navbar flex justify-center bg-base-100 shadow-sm h-14 py-0">
-        <div className="w-screen max-w-7xl px-2 h-full">
-          <div className="navbar-start">
-            <Link href={'/'} >Aaron Arquette</Link>
-          </div>
-          <div className="navbar-center">
-
-          </div>
-          <div className="navbar-end h-full">
-            <Link className="btn btn-ghost h-full" href={'/about'}>About</Link>
-            <Link className="btn btn-ghost h-full" href={'/projects'}>Projects</Link>
-            <Link className="btn btn-ghost h-full" href={'/tech-stacks'}>Tech Stacks</Link>
-            <Link className="btn btn-ghost h-full" href={'/contact'}>Contact Me</Link>
-          </div>
+      <nav className="navbar fixed z-50 justify-center bg-base-200/5 backdrop-blur-sm shadow-md h-14 py-0">
+        <div className="navbar-start h-full justify-center">
+          <Link className="" href={'/'} >Aaron Arquette</Link>
+        </div>
+        <div className="navbar-center h-full">
+          <Link className="btn btn-ghost h-full" href={'/about'}>About</Link>
+          <Link className="btn btn-ghost h-full" href={'/projects'}>Projects</Link>
+          <Link className="btn btn-ghost h-full" href={'/tech-stacks'}>Tech Stacks</Link>
+        </div>
+        <div className="navbar-end h-full justify-center">
+          <Link className="btn btn-ghost h-full" href={'/contact'}>Contact Me</Link>
         </div>
       </nav>
     </>
